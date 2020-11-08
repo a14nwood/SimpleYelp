89426c9f512c751db3d611ad9dafaf3e4a8a5e85
Michael Wood <58078180+micalawoo@users.noreply.github.com>
2020-11-07 17:35:36 -0700

    Create data classes

--- app/src/main/java/edu/stanford/mialwojr/simpleyelp/MainActivity.kt
+++ app/src/main/java/edu/stanford/mialwojr/simpleyelp/MainActivity.kt
@@ -24,12 +24,12 @@ class MainActivity : AppCompatActivity() {
                 .build()
         val yelpService = retrofit.create(YelpService::class.java)
         yelpService.searchRestaurants("Bearer $API_KEY", "Avocado Toast", "New York")
-            .enqueue(object : Callback<Any> {
-                override fun onResponse(call: Call<Any>, response: Response<Any>) {
+            .enqueue(object : Callback<YelpSearchResult> {
+                override fun onResponse(call: Call<YelpSearchResult>, response: Response<YelpSearchResult>) {
                     Log.i(TAG, "onResponse $response")
                 }
 
-                override fun onFailure(call: Call<Any>, t: Throwable) {
+                override fun onFailure(call: Call<YelpSearchResult>, t: Throwable) {
                     Log.i(TAG, "onFailure $t")
                 }
 
--- app/src/main/java/edu/stanford/mialwojr/simpleyelp/YelpService.kt
+++ app/src/main/java/edu/stanford/mialwojr/simpleyelp/YelpService.kt
@@ -12,5 +12,5 @@ public interface YelpService {
         @Header("Authorization") authHeader: String,
         @Query("term") searchTerm: String,
         @Query("location") location: String
-    ) : Call<Any>
+    ) : Call<YelpSearchResult>
 }
